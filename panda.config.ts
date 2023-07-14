0f45d16b81d3fd02b204c4ed52983fbf3474511c
しまぶー <0120koto@gmail.com>
2023-07-14 18:33:55 +0900

    🐛 hashがまだ機能していないので一旦削除

--- panda.config.ts
+++ panda.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
   exclude: [],
   outdir: "styled-system",
-  hash: process.env.NODE_ENV === "production",
   theme: {
     extend: {},
   },
